12880e657564ec589799800ff803630efca9e512
Biel Serrano Sanchez <bielsesa@gmail.com>
2019-10-15 12:21:57 +0200

    Commit inicial

---
+++ Calendario/DiaCalendario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calendario
+{
+    public class DiaCalendario
+    {
+        private DateTime fecha;
+        private bool esFestivo;
+
+
+        public DateTime Fecha { get { return fecha;  }  set { fecha = value; } }
+        public bool EsFestivo { get { return esFestivo;  }  set { esFestivo = value; } }
+
+        public DiaCalendario(DateTime fecha, bool esFestivo)
+        {
+            Fecha = fecha;
+            EsFestivo = esFestivo;
+        }
+
+        public override string ToString()
+        {
+            return fecha.Day.ToString();
+        }
+    }
+}
---
+++ Calendario/VentanaCalendario.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Calendario
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaCalendario.xaml
+    /// </summary>
+    public partial class VentanaCalendario : Window
+    {
+        public VentanaCalendario()
+        {
+            InitializeComponent(); 
+            ListaDias = new ObservableCollection<DiaCalendario>();
+
+            for (int i = 0; i < 42; i += 2)
+            {
+                ListaDias.Add(new DiaCalendario(DateTime.Now, true));
+                ListaDias.Add(new DiaCalendario(DateTime.Now, false));
+            }
+
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<DiaCalendario> ListaDias
+        {
+            get;
+            set;
+        }
+
+        private void LabelDiaLaborable_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void LabelDiaFestivo_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
